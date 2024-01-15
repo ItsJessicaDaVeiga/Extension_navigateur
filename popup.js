4db935b2b270b3131e821cc91c39739796a25c93
maxlebro <z.joriot@gmail.com>
2024-01-15 14:43:31 +0100

    ajout minuteur

--- popup.js
+++ popup.js
@@ -48,19 +48,19 @@ const affirmations = [
   "You're an inspiration",
   "Your life is already a miracle of chance waiting for you to shape its destiny",
   "Your life is about to be incredible",
-  "Nothing is impossible. The word itself says 'I’m possible!'",
+  "Nothing is impossible. The word itself says 'I'm possible!'",
   "Failure is just another way to learn how to do something right",
   "I give myself permission to do what is right for me",
   "You can do it",
   "It is not a sprint, it is a marathon. One step at a time",
   "Success is the progressive realization of a worthy goal",
-  "People with goals succeed because they know where they’re going",
+  "People with goals succeed because they know where they're going",
   "All you need is the plan, the roadmap, and the courage to press on to your destination",
   "The opposite of courage in our society is not cowardice... it is conformity",
-  "Whenever we’re afraid, it’s because we don’t know enough. If we understood enough, we would never be afraid",
+  "Whenever we're afraid, it's because we don't know enough. If we understood enough, we would never be afraid",
   "The past does not equal the future",
   "The path to success is to take massive, determined action",
-  "It’s what you practice in private that you will be rewarded for in public",
+  "It's what you practice in private that you will be rewarded for in public",
   "Small progress is still progress",
   "Don't worry if you find flaws in your past creations, it's because you've evolved",
   "Starting is the most difficult step - but you can do it",
@@ -78,4 +78,51 @@ document.addEventListener("DOMContentLoaded", function () {
   citationContainer.textContent = citation;
 });
 
-fetchData("https://www.affirmations.dev/");
+//pour minuteur
+
+let intervalId;
+
+function decompte() {
+  if (startButton.textContent === "start") {
+    startButton.textContent = "stop";
+    const departMinutes = timerElement.textContent.match(/\d+/g);
+    let temps =
+      parseInt(departMinutes[0]) * 60 + parseInt(departMinutes[1]) - 1;
+    intervalId = setInterval(() => {
+      let minutes = parseInt(temps / 60, 10);
+      let secondes = parseInt(temps % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      secondes = secondes < 10 ? "0" + secondes : secondes;
+
+      timerElement.innerText = `${minutes} : ${secondes}`;
+      temps = temps <= 0 ? 0 : temps - 1;
+
+      if (temps === 0) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  } else {
+    startButton.textContent = "start";
+    clearInterval(intervalId);
+  }
+}
+
+const timerElement = document.getElementById("number");
+const upArrow = document.querySelectorAll(".up");
+const startButton = document.getElementById("startButton");
+const downArrow = document.querySelectorAll(".down");
+
+upArrow.forEach(function (arrow) {
+  arrow.addEventListener("click", function () {
+    timerElement.textContent = parseInt(timerElement.textContent) + 1 + " : 00";
+  });
+});
+
+downArrow.forEach(function (arrow) {
+  arrow.addEventListener("click", function () {
+    timerElement.textContent = parseInt(timerElement.textContent) - 1 + " : 00";
+  });
+});
+
+startButton.addEventListener("click", decompte);
